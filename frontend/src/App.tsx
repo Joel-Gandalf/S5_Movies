4de965a9bc4689499f6105a4da94801ad73ee412
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-08-29 14:55:46 +0200

    style(navbar): add Navbar responsive styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
-import styles from './App.module.css'
 import { AppRoutes } from './routes/AppRoutes'
 
 export const App = () => {
 
     return (
-        <div className={styles.container}>
-            <AppRoutes />
-        </div> 
+            <AppRoutes /> 
     )
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,14 @@
 import { navLinkItems } from "../config/navLinkItems";
 import { NavLink } from "react-router";
+import styles from '../styles/Navbar.module.css';
 
 export const Navbar = () => {
 
     return (
-        <ul>
+        <ul className={styles.list}>
             {navLinkItems.map(link => (
                 <li key={link.id}>
-                    <NavLink to={link.path} aria-label={link.ariaLabel}>
+                    <NavLink to={link.path} aria-label={link.ariaLabel} className={styles.link}>
                         {link.label}
                     </NavLink>
                 </li>
