961c8f05800177d569d4cb09ba4b64e2fbe813e8
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-08-29 13:54:22 +0200

    style(navbar): add AccountNav responsive styles

--- frontend/src/components/AccountNav.tsx
+++ frontend/src/components/AccountNav.tsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router";
 import { useAuthPlaceholder } from "../hooks/useAuthPlaceholder";
+import styles from '../styles/AccountNav.module.css';
 
 export const AccountNav = () => {
 
@@ -11,18 +12,22 @@ export const AccountNav = () => {
             
             {isLoggedIn ? (
                 <div>
-                    <NavLink to="/account" aria-label="Ir a mi cuenta">
+                    <NavLink to="/account" aria-label="Ir a mi cuenta" className={styles.accountLink}>
                         {userName} Mi cuenta
                     </NavLink>
                 </div >
             ) : (
 
-                <ul>
+                <ul className={styles.list}>
                     <li>
-                        <NavLink to="/login" aria-label="Ir a iniciar sesión">Iniciar sesión</NavLink>
+                        <NavLink to="/login" aria-label="Ir a iniciar sesión" className={styles.link}>
+                            Iniciar sesión
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/register" aria-label="Ir a registrarse">Registrarse</NavLink>
+                        <NavLink to="/register" aria-label="Ir a registrarse" className={styles.link}>
+                            Registrarse
+                        </NavLink>
                     </li>
                 </ul>
             )}
