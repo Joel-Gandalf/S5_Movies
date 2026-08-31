7bc69c8a5baafe30fff34a35211131a9ed3d95be
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-08-31 19:00:58 +0200

    feat(config): add comments to tmdbConfig.ts

--- frontend/src/config/tmdbConfig.ts
+++ frontend/src/config/tmdbConfig.ts
@@ -1,13 +1,16 @@
+// urls para endpoints
 export const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 export const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
+
 export const POSTER_SIZE = 'w342';
+// query params
 export const DEFAULT_DISCOVER_PARAMS = {
   sort_by: 'primary_release_date.desc',
   region: 'ES',
   include_adult: false,
 };
+// primary_release_date.lte=fecha_actual
+// Obtención de la fecha actual:
 export const getTodayFormatted = (): string => {
   return new Date().toISOString().slice(0, 10);
 };
-
-// primary_release_date.lte=fecha_actual
