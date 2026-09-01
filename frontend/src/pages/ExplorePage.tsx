40630958e11adfb1b63a24dd4a7ce369ef49d8d2
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-09-01 20:41:02 +0200

    feat(ui): add ExplorePage base structure with mock data

--- frontend/src/pages/ExplorePage.tsx
+++ frontend/src/pages/ExplorePage.tsx
@@ -1,5 +1,18 @@
+import { mockMovies } from "./mockMovies";
+import { MovieGrid } from "../components/MovieGrid";
+import { Pagination } from "../components/Pagination";
+
 export const ExplorePage = () => {
+
+    const handlePageChange = () => {
+
+    } 
+
     return (
-        <></>
+        <>
+            <h1>Novedades para descubrir</h1>
+            <MovieGrid movies={mockMovies} />
+            <Pagination currentPage={1} totalPages={5} onPageChange={handlePageChange} />
+        </>
     );
 }
