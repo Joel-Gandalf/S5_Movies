7a3b4281d3b574ebc7edaf213a96bc752acf4eff
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-09-01 17:49:51 +0200

    refactor(ui): use content.json for LoadingSpinner text

--- frontend/src/components/LoadingSpinner.tsx
+++ frontend/src/components/LoadingSpinner.tsx
@@ -1,9 +1,11 @@
+import content from "../config/content.json"
+
 export const LoadingSpinner = () => {
 
     return (
 
         <div role="status" aria-live="polite">
-            <span className="sr-only">Cargando...</span>
+            <span className="sr-only">{content.common.loading}</span>
         </div>
     );
 }
