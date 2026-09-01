a4a18e9950ede5407e41b2e264e211a57ce39b62
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-09-01 17:37:14 +0200

    feat(ui): add LoadingSpinner component

---
+++ frontend/src/components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+export const LoadingSpinner = () => {
+
+    return (
+
+        <div role="status" aria-live="polite">
+            <span className="sr-only">Cargando...</span>
+        </div>
+    );
+}
