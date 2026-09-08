4bfc6a3e3d58c6bb19b2cfdd9901c89ae7c68714
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-09-08 22:34:39 +0200

    refactor(utils): make poster size configurable in getPosterUrl

--- frontend/src/utils/getPosterUrl.ts
+++ frontend/src/utils/getPosterUrl.ts
@@ -2,10 +2,10 @@ import { getImageUrl } from "./getImageUrl";
 import { TMDB_IMAGE_BASE_URL, POSTER_SIZE } from "../config/tmdbConfig";
 import noPosterImage from "../assets/icons/no-fotos.png";
 
-export const getPosterUrl = (posterPath: string | null): string =>
+export const getPosterUrl = (posterPath: string | null, size: string = POSTER_SIZE): string =>
   getImageUrl({
     baseUrl: TMDB_IMAGE_BASE_URL,
     imagePath: posterPath,
-    imageSize: POSTER_SIZE,
+    imageSize: size,
     placeholder: noPosterImage,
   });
