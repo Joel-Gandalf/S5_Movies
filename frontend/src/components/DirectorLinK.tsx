89c4c16acef8c498bc2560047597175d6f836de2
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-09-15 01:30:07 +0200

    style(components): add DirectorLink styles

--- frontend/src/components/DirectorLinK.tsx
+++ frontend/src/components/DirectorLinK.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 import type { CrewMember } from "../types/CrewMember";
-import content from "../config/content.json"
+import content from "../config/content.json";
+import styles from "../styles/DirectorLink.module.css";
 
 interface DirectorLinkProps {
     director: CrewMember | undefined;
@@ -15,8 +16,9 @@ export const DirectorLink = ({director}: DirectorLinkProps) => {
 
     return(
 
-        <Link to={`/people/${director.id}`}>
-            <p>{`Director/a: ${director.name}`}</p>
+        <Link to={`/people/${director.id}`} className={styles.link}>
+            <p className={styles.label}>Director/a: </p>
+            <p className={styles.name}>{director.name}</p>
         </Link>
     );
 }
