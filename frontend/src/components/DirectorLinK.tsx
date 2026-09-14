66acb0d9d348c985ff0852c0a9060f15e06ac87f
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-09-14 22:02:46 +0200

    feat(components): add DirectorLink component

---
+++ frontend/src/components/DirectorLinK.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router";
+import type { CrewMember } from "../types/CrewMember";
+import content from "../config/content.json"
+
+interface DirectorLinkProps {
+    director: CrewMember | undefined;
+}
+
+export const DirectorLink = ({director}: DirectorLinkProps) => {
+    
+
+    if (!director) {
+        return <p>{content.movieDetail.notInfo}</p>;
+    }
+
+    return(
+
+        <Link to={`/people/${director.id}`}>
+            <p>{director.name}</p>
+        </Link>
+    );
+}
