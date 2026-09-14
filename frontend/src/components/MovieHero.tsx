ba3312524df4c0c864fd9b275d8ae9cb998d984e
Joel-Gandalf <joelvsgandalf@gmail.com>
2026-09-14 23:22:50 +0200

    feat(components): add MovieHero component with poster, synopsis and genres

---
+++ frontend/src/components/MovieHero.tsx
@@ -0,0 +1,31 @@
+import { getPosterUrl } from "../utils/getPosterUrl";
+import type { MovieDetail } from "../types/MovieDetail";
+
+interface MovieHeroProps {
+    movie: MovieDetail;
+}
+
+export const MovieHero = ({ movie }: MovieHeroProps) => {
+    
+    const releaseYear = movie.release_date.slice(0, 4);
+
+    return (
+        <section aria-labelledby="movie-title">
+            <img src={getPosterUrl(movie.poster_path)} alt={`Póster de ${movie.title}`} />
+
+            <div>
+                <h1 id="movie-title">{movie.title}</h1>
+                <p>{movie.overview}</p>
+                <p>Año de estreno: {releaseYear}</p>
+
+                <ul>
+                    {movie.genres.map(genre => (
+                        <li key={genre.id}>
+                            <p>{genre.name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    );
+}
